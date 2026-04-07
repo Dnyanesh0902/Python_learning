b3b3beb72179217bd2e28b3af015f87057e4d224
dnyaneshwarkokate <Dnyaneshwarkokate@@electropneumatics.com>
2026-04-07 14:32:34 +0530

    first commit

---
+++ Day06-Python-Tuples/tuples.py
@@ -0,0 +1,16 @@
+# Program 1 - Create Tuple
+numbers = (10, 20, 30)
+print(numbers)
+
+
+# Program 2 - Access Element
+
+colors = ("Red","Green","Blue")
+print(colors[1])
+
+# Program 3 -Loop Through Tuple 
+
+fruits = ("apple", "banana", "mango")
+
+for fruit in fruits:
+    print(fruit)
