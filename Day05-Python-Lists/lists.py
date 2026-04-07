2e7b7b543fd1d8d195822bfe2c086e60ae78e41c
dnyaneshwarkokate <Dnyaneshwarkokate@@electropneumatics.com>
2026-04-07 12:55:42 +0530

    first commit

---
+++ Day05-Python-Lists/lists.py
@@ -0,0 +1,22 @@
+# Program 1 -Create List
+names = ["Dnyanesh","Kiran","Vaibhav"]
+
+print(names)
+
+# Progarm 2 -- Add Item
+
+names =["Dnyanesh","Kiran"]
+names.append("Ashish")
+print(names)
+
+# Program 3 - Print All Elements
+
+numbers = [10, 20, 30, 40]
+
+for num in numbers:
+    print(num)
+
+# Program 4 - find length
+
+Students = ["Amit","Rohit","Rahul"]
+print(len(Students))
